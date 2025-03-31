84f747c651830d61b2b0c8690648e0203d7bec8d
deirUwU3 <dale25alfaro@gmail.com>
2025-03-31 08:52:40 +0800

    Add project files.

---
+++ pokemonactivity/Controllers/Pokemoncontroller.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pokemonactivity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Pokemoncontroller : ControllerBase
+    {
+
+        public Pokemoncontroller()
+        {
+            pokemonrep.Initialize();
+        }
+
+        [HttpGet]
+        [Route("get-all")]
+        public IActionResult GetAction()
+        {
+
+            return Ok(pokemonrep.GetPokemonList());
+        }
+
+        [HttpGet]
+        public IActionResult GetPokemon(string? name)
+        {
+            if (name == null) return Ok("not okay!");
+            var result = pokemonrep.GetPokemonList()
+                .Where(x => name.ToLower() == x.Name.ToLower())
+                .SingleOrDefault();
+
+            if (result != null) return Ok(result);
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Pokemon pokemon)
+        {
+            pokemonrep.AddPokemon(pokemon);
+            return Created("", null);
+        }
+    }
+
+}
---
+++ pokemonactivity/pokemonrep.cs
@@ -0,0 +1,33 @@
+﻿namespace pokemonactivity
+{
+    public class pokemonrep
+    {
+        
+        private static List<Pokemon> _pokemons = new List<Pokemon>();
+
+        public static void Initialize()
+        {
+            _pokemons.Add(new Pokemon
+            {
+                Name = "Balbasaus",
+                Type = "Grass",
+                Height = 0.7f,
+                Weight = 6.9f,
+            });
+        }
+
+        public static List<Pokemon> GetPokemonList()
+        {
+            if (_pokemons.Count == 0 || _pokemons.Count() == 0)
+            {
+                Initialize();
+            }
+            return _pokemons;
+        }
+
+        public static void AddPokemon(Pokemon pokemon)
+        {
+            _pokemons.Add(pokemon);
+        }
+    }
+}
